71a559f0fd28fced9f9f47b227820558d7d7ca51
nsoftnhdev <n.softnhdev@gmail.com>
2026-08-18 09:44:25 +0800

    add authentication routes and middleware for user verification

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import job from "./lib/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -27,6 +28,8 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+app.use("/api/auth", authRoutes);
 
 // if the public directory exists, serve the static files
 // This is for the production build
